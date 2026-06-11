2d818ded05a56843a1fea1b88ac392207f8195ca
Lucas <89300875+torols@users.noreply.github.com>
2026-06-11 17:50:30 -0300

    Add files via upload

--- pulse_system_dll.cs
+++ pulse_system_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-06-02 23:56:17.631702900 UTC
+// 2026-06-11 20:48:41.757994200 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: pulse_system.dll
