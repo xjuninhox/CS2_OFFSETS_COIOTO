28d04d73c654ce771efb2d86392d6ef821cd563e
xjuninhox <github@coiotojr.shop>
2026-06-29 21:50:18 -0300

    Add files via upload

--- vphysics2_dll.cs
+++ vphysics2_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-06-11 20:48:41.757994200 UTC
+// 2026-06-30 00:44:10.586393700 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: vphysics2.dll
