c5f83f6184f9896bbf62f5b5c5648dcfa869e688
Rox079 <164399489+Rox079@users.noreply.github.com>
2026-08-13 15:09:58 -0300

    Add files via upload

--- vphysics2_dll.zig
+++ vphysics2_dll.zig
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-08-12 23:01:03.020832500 UTC
+// 2026-08-13 18:06:45.810888800 UTC
 
 pub const cs2_dumper = struct {
     pub const schemas = struct {
